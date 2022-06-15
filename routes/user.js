f47c163f98090b90946910848e147bdd66b19fe6
ShaharWeisberger <81864845+ShaharWeisberger@users.noreply.github.com>
2022-06-15 15:09:24 +0300

    user-2

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,8 @@ const DButils = require("./utils/DButils");
 const user_utils = require("./utils/user_utils");
 const recipe_utils = require("./utils/recipes_utils");
 
-/* Authenticate all incoming requests by middleware */
+//Authenticate all incoming requests by middleware
+
 router.use(async function (req, res, next) {
   if (req.session && req.session.user_id) {
     DButils.execQuery("SELECT user_id FROM users").then((users) => {
@@ -20,9 +21,10 @@ router.use(async function (req, res, next) {
   }
 });
 
+router.get("/alive1", (req, res) => res.send("I'm alive1"));
 
 /* This path gets body with recipeId and save this recipe in the favorites list of the logged-in user */
-router.post('/profile/addFavoriteRecipe', async (req, res, next) => {
+router.post('/addFavoriteRecipe', async (req, res, next) => {
   try 
   {
     const user_id = req.session.user_id;
@@ -36,100 +38,38 @@ router.post('/profile/addFavoriteRecipe', async (req, res, next) => {
   }
 })
 
-/* This path returns the favorites recipes that were saved by the logged-in user */
-router.get('/favorites', async (req, res, next) => {
-  try 
-  {
-    const user_id = req.session.user_id;
-    let favorite_recipes = {};
-    const recipes_id = await user_utils.getFavoriteRecipes(user_id);
-    let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
-    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
-    res.status(200).send(results);
-  } 
-  catch (error) 
-  {
-    next(error);
-  }
-});
-
-/* User-Acount */
-/* Register */
-router.post('/account/register', async (req, res, next) => {
-  try 
-  {
-    const { username, firstname, lastname, coutry, password, email } = req.body;
-    let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
-    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
-    res.status(200).send(results);
-  } 
-  catch (error) 
-  {
-    next(error);
-  }
-});
-
-/* Login */
-router.post('/account/login', async (req, res, next) => {
-  try 
-  {
-    const { username, firstname, lastname, coutry, password, email } = req.body;
-    let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
-    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
-    res.status(200).send(results);
-  } 
-  catch (error) 
-  {
-    next(error);
-  }
-});
-
-/* LogOut */
-router.post('/account/logout', async (req, res, next) => {
-  try 
-  {
-    const { username, firstName, lastName, coutry, password, email } = req.body;
-    let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
-    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
-    res.status(200).send(results);
-  } 
-  catch (error) 
-  {
-    next(error);
-  }
-});
-
 /* Get-Favorite-Recipe */
- router.get('/profile/getFavouriteRecipe', async (req, res, next) => {
+router.get('/getFavouriteRecipe', async (req, res, next) => {
   try 
   {
     const user_id = req.session.user_id;
-    let favorite_recipes = {};
+    //let favorite_recipes = {};
     const recipes_id = await user_utils.getFavoriteRecipes(user_id);
     let recipes_id_array = [];
     recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
     const results = await recipe_utils.getRecipesPreview(recipes_id_array);
     res.status(200).send(results);
   } 
-  catch (error) 
+  catch (error)
   {
     next(error);
   }
 });
 
 /* Add-Recipe */
-router.post('/profile/addRecipe', async (req, res, next) => {
+router.post('/addRecipe', async (req, res, next) => {
   try 
   {
-    const { username, firstName, lastName, coutry, password, email } = req.body;
-    let recipes_id_array = [];
-    recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
-    const results = await recipe_utils.getRecipesPreview(recipes_id_array);
-    res.status(200).send(results);
+    const user_id = req.session.user_id;
+    const image = req.body.image;
+    const name = req.body.name;
+    const likes = req.body.likes;
+    const vegetarian = req.body.vegetarian;
+    const vegan = req.body.vegan;
+    const gluten = req.body.glutenFree;
+    const favorite = req.body.userFavorite;
+    await user_utils.insertNewRecipe(user_id, image, name, likes, vegetarian, vegan, gluten, favorite);
+    res.status(200).send("The Recipe successfully saved in users profile!#!#!#!");
   } 
   catch (error) 
   {
@@ -138,18 +78,18 @@ router.post('/profile/addRecipe', async (req, res, next) => {
 });
 
 /* Get-Recipes */
- router.get('/profile/getRecipe', async (req, res, next) => {
+ router.get('/getRecipe', async (req, res, next) => {
   try 
   {
     const user_id = req.session.user_id;
-    let favorite_recipes = {};
-    const recipes_id = await user_utils.getFavoriteRecipes(user_id);
+    //let favorite_recipes = {};
+    const recipes_id = await user_utils.getMyRecipes(user_id);
     let recipes_id_array = [];
     recipes_id.map((element) => recipes_id_array.push(element.recipe_id)); //extracting the recipe ids into array
     const results = await recipe_utils.getRecipesPreview(recipes_id_array);
     res.status(200).send(results);
   } 
-  catch (error) 
+  catch (error)
   {
     next(error);
   }
